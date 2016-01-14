7c50dcfa9e9b74d8d166f5cd4c07298d38a46dd3
cln <seinc3@hotmail.com>
2016-01-14 08:36:35 +0100

    add new page for transaction

--- src/C3/BudgetBundle/Controller/TransactionController.php
+++ src/C3/BudgetBundle/Controller/TransactionController.php
@@ -3,6 +3,10 @@
 namespace C3\BudgetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use C3\BudgetBundle\Entity\Transaction;
 
 class TransactionController extends Controller
@@ -23,5 +27,24 @@ class TransactionController extends Controller
         $transactions = $repository->findAll();
 
         return $this->render('C3BudgetBundle:Transaction:index.html.twig', array('transactions' => $transactions));
+    }
+
+    public function addAction()
+    {
+        $transaction = new Transaction();
+        $transaction->setDate(new \DateTime('14-02-2012', new \DateTimeZone('Europe/Paris')));
+        $transaction->setAmount('19.99');
+        $transaction->setDescription('Lorem ipsum dolor');
+
+        $form = $this->createFormBuilder($transaction)
+            ->add('date', 'date')
+            ->add('amount', 'money')
+            ->add('description', 'text')
+            ->add('save', 'submit', array('label' => 'Créer une transaction'))
+            ->getForm();
+
+        return $this->render('C3BudgetBundle:Transaction:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
